6a081cd2f3365181fe272b534ad82f924768826b
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-08-30 09:13:47 -0400

    first

---
+++ api/models/Users.js
@@ -0,0 +1,17 @@
+/**
+ * Users
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+  	userID: 'INT',
+  	username: 'STRING',
+  	password: 'STRING'
+  }
+
+};
