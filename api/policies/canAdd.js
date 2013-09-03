12dcfd8675a2c67bae088e1debf7e3689e4dbdbe
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-09-03 13:37:00 -0400

    forgot to add the actual file

---
+++ api/policies/canAdd.js
@@ -0,0 +1,26 @@
+module.exports = function(req,res,next) {
+	if (req.param('gender') == 'F') {
+		next();
+	} else {
+		var guyCount = 0, girlCount = 0;
+		Guests.findByAddedBy(req.session.user.username).done(function(err,guestsAddedByUser){
+			for(var i = 0 ; i < guestsAddedByUser.length; i++){
+				guestsAddedByUser[i].gender == 'M' ? guyCount++ : girlCount++;
+			}
+		});
+
+		Configs.findByOrganization('SigmaPiGammaIota').done(function(err,conf){
+			conf = conf[0];
+			if (err) {
+				next(); //no configs to check, pass on
+			}
+			var minGirls = conf.minimumGirlRatio;
+			if (minGirls > 0) {
+				var allowable = Math.floor(girlCount / minGirls);
+				if (guyCount < allowable) {
+					next();
+				}
+			}
+		});
+	}
+};
