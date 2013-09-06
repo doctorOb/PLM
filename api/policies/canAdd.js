2a20407f1166406736293fe9565b8c9bc23d52c7
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-09-06 13:20:56 -0400

    fixed dudes not getting removed...something to do with the policy chaining...idk it works now

--- api/policies/canAdd.js
+++ api/policies/canAdd.js
@@ -10,32 +10,31 @@ module.exports = function(req,res,next) {
 	if (isDuplicate) return;
 
 	if (req.param('gender') == 'F') {
-		next();
+		return next();
 	} else {
 		var guyCount = 0, girlCount = 0;
 		Guests.findByAddedBy(req.session.user.username).done(function(err,guestsAddedByUser){
 			for(var i = 0 ; i < guestsAddedByUser.length; i++){
 				guestsAddedByUser[i].gender == 'M' ? guyCount++ : girlCount++;
 			}
-		});
-
-		Configs.findByOrganization('SigmaPiGammaIota').done(function(err,conf){
-			conf = conf[0];
-			if (err) {
-				next(); //no configs to check, pass on
-			}
-			var ratio = conf.minimumGirlRatio;
-			if (ratio > 0) {
-				var allowable = Math.floor(girlCount / ratio);
-				if (guyCount < allowable) {
-					next();
-				} else {
-					res.send(400, 'must have a girl:guy ratio of ' + ratio + ':1');
+			Configs.findByOrganization('SigmaPiGammaIota').done(function(err,conf){
+				conf = conf[0];
+				if (err) {
+					next(); //no configs to check, pass on
 				}
-			}
-			else {
-				next();
-			}
+				var ratio = conf.minimumGirlRatio;
+				if (ratio > 0) {
+					var allowable = Math.floor(girlCount / ratio);
+					if (guyCount < allowable) {
+						next();
+					} else {
+						return res.send(400, 'must have a girl:guy ratio of ' + ratio + ':1');
+					}
+				}
+				else {
+					next();
+				}
+			});
 		});
 	}
 
--- api/policies/canRemove.js
+++ api/policies/canRemove.js
@@ -5,28 +5,28 @@ module.exports = function(req,res,next) {
 		for(var i = 0 ; i < guestsAddedByUser.length; i++){
 			guestsAddedByUser[i].gender == 'M' ? guyCount++ : girlCount++;
 			if (guestsAddedByUser[i].id == req.param('id') && guestsAddedByUser[i].gender == 'M') {
-				next();
-			}
+				return next();
+			}	
 		}
-	});
 
-	Configs.findByOrganization('SigmaPiGammaIota').done(function(err,conf){
-		conf = conf[0];
-		if (err) {
-			next(); //no configs to check, pass on
-		}
-		var ratio = conf.minimumGirlRatio;
-		if (ratio > 0) {
-			var allowable = Math.ceil(guyCount * ratio);
-			if (girlCount > allowable) {
-				next();
-			} else {
-				res.send(400, 'must have a girl:guy ratio of ' + ratio + ':1');
+		Configs.findByOrganization('SigmaPiGammaIota').done(function(err,conf){
+			conf = conf[0];
+			if (err) {
+				return next(); //no configs to check, pass on
 			}
-		}
-		else {
-			next();
-		}
+			var ratio = conf.minimumGirlRatio;
+			if (ratio > 0) {
+				var allowable = Math.ceil(guyCount * ratio);
+				if (girlCount > allowable) {
+					next();
+				} else {
+					return res.send(400, 'must have a girl:guy ratio of ' + ratio + ':1');
+				}
+			}
+			else {
+				return next();
+			}
+		});
 	});
 
 };
